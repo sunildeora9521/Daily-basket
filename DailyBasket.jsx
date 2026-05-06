a02de2290db5f7cf208567f36408543c78e117b2
sunildeora9521 <sunildeora9521@gmail.com>
2026-05-06 13:54:11 +0530

    Update DailyBasket.jsx

--- DailyBasket.jsx
+++ DailyBasket.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect, useRef } from "react";
-
+import { useState, useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "./Login";
 /* ═══════════ GLOBAL CSS ═══════════ */
 const CSS = `
 @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@700;800&family=Noto+Sans+Devanagari:wght@400;600;700;800&display=swap');
