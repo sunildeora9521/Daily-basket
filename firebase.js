32deac64956e591dbb0bfbc030c472b460e7d341
sunildeora9521 <sunildeora9521@gmail.com>
2026-05-06 13:51:34 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2AKNq6YSU_NYMU7eSYC8Gm-o84SDW99E",
+  authDomain: "daily-basket-25d1a.firebaseapp.com",
+  projectId: "daily-basket-25d1a",
+  storageBucket: "daily-basket-25d1a.firebasestorage.app",
+  messagingSenderId: "852431532900",
+  appId: "1:852431532900:web:abb9ceb9c49586a4934071"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
