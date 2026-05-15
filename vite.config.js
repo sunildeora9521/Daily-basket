f55c78104489afd1b8f9efac77c5c35f579a3320
sunildeora9521 <sunildeora9521@gmail.com>
2026-05-15 11:21:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,10 +25,9 @@ start_url: './',
           { src: '/icon-512.png', sizes: '512x512', type: 'image/png', form_factor: 'narrow', label: 'Daily Basket Home' }
         ],
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' }
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       }
     })
